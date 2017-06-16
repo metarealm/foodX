a30432794eeb398cb87f0cc1801056cd80696a29
bhabani.das@gmail.com <bhabani.das@gmail.com>
2017-06-15 21:14:34 -0400

    delete doc folder

--- docs/out-tsc/app/shared/constants.js
+++
@@ -1,3 +0,0 @@
-export var YOUTUBE_API_KEY = 'AIzaSyBgJ0KTiDr4VQKrAJ9-BmR7oMFMivAksEc';
-export var SOLR_SERVER_PATH = 'http://10.0.0.16:8983/solr/#/';
-//# sourceMappingURL=../../../../src/app/shared/constants.js.map
