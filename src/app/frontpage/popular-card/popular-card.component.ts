42cf506fa6fbf430dc8880c64783ef2c9fc67847
bhabani.das@gmail.com <bhabani.das@gmail.com>
2017-08-01 19:32:17 -0400

    change icon

---
+++ src/app/frontpage/popular-card/popular-card.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'popularcard',
+  templateUrl: './popular-card.component.html',
+  styleUrls: ['./popular-card.component.css']
+})
+export class PopularcardComponent {
+
+}
