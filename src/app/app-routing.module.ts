52f69d707f567efe04902038aa8cf33b0adff321
bhabani.das@gmail.com <bhabani.das@gmail.com>
2018-03-03 19:50:45 -0800

    change something

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { RouterModule, Routes }  from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { FrontpageComponent }     from './frontpage/frontpage.component';
 import { PageNotFoundComponent } from './shared/components/not-found.component';
-<<<<<<< HEAD
-import { LoginCallbackComponent } from './frontpage/login-callback/login-callback.component'
-=======
 import { LoginCallbackComponent } from './shared/components/login-callback.component'
->>>>>>> 61882077ce05737f71338392af777078a8b1095e
  
 const appRoutes: Routes = [
   { path: '',component: FrontpageComponent},
